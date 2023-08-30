ebffe2028ab44fd7ab210d0d478d8891c547527d
patrickbyteflow <odeydev@gmail.com>
2023-08-30 11:25:32 +0100

    Initial commit

--- apps/account/src/account.service.ts
+++ apps/account/src/account.service.ts
@@ -19,7 +19,26 @@ export class AccountService {
   }
 
   async getUserDetailsByAccountNumber(accountNumber: string): Promise<any> {    
-    let data = `<?xml version="1.0" encoding="utf-8"?>\n<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">\n  <soap:Header>\n    <Authenticate xmlns="http://tempuri.org/">\n      <userName>CELLULAT-0100</userName>\n      <password>infosight!12</password>\n    </Authenticate>\n  </soap:Header>\n  <soap:Body>\n    <AccountListAll xmlns="http://tempuri.org/">\n      <AccountAll>\n        <AccountNo>${accountNumber}</AccountNo>\n      </AccountAll>\n    </AccountListAll>\n  </soap:Body>\n</soap:Envelope>`;
+    let data = 
+    `<?xml version="1.0" encoding="utf-8"?>
+      <soap:Envelope 
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
+        xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Header>
+          <Authenticate xmlns="http://tempuri.org/">
+            <userName>CELLULAT-0100</userName>
+            <password>infosight!12</password>
+          </Authenticate>
+        </soap:Header>
+        <soap:Body>
+          <AccountListAll xmlns="http://tempuri.org/">
+            <AccountAll>
+              <AccountNo>${accountNumber}</AccountNo>
+            </AccountAll>
+          </AccountListAll>
+        </soap:Body>
+      </soap:Envelope>`;
     const response = await this.bankingService.fetchBankingAccounts(data);
     return response;
   }
